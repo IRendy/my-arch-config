2b6602e7fe33af4d09ee3d851ef76f84184e35a6
IRendy <326821729@qq.com>
2025-09-01 17:08:48 +0800

    first commit

---
+++ nvim/README.md
@@ -0,0 +1,14 @@
+# Neovim 配置说明
+
+## 基本说明
+
+该项目基于 [LazyVim](https://github.com/LazyVim/LazyVim)，并添加了一些插件和自定义的配置。
+部分配置参考了 [pdcxs的Neovim配置](https://github.com/pdcxs/nvim)。
+
+## 安装方法
+
+```zsh
+git clone https://github.com/IRendy/nvim-config.git
+```
+
+打开`nvim`，自动安装
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,15 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+if vim.g.neovide then
+  vim.o.guifont = "CaskaydiaCove Nerd Font:h15"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_scale_factor = 0.7
+  -- turn off animate because we are using neovide
+  vim.g.snackes_animate = true
+end
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencodings = "utf-8,gb2312,gbk"
+vim.o.mouse = "nv"
